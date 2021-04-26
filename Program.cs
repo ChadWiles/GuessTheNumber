e9ef74e5d8da30df44d0e6339203b73978364b31
Chad <103564240@student.swin.edu.au>
2021-04-26 13:02:59 +1000

    Started over; reviewed and simplified

--- Program.cs
+++ Program.cs
@@ -4,20 +4,33 @@ namespace GuessTheNumber
 {
     class Program
     {
-        private static int GetGuess()
+        static void Main(string[] args)
         {
             int guess = 0;
-            try
-            {  
-            guess = Convert.ToInt32(Console.ReadLine());
+            string welcome = "Guess a number between 1 and 25";
+            int num = rand.Next(1,25);
+            Console.WriteLine(welcome);
+
+            int i = 0;
+
+            while guess != num)
+            {   
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess > num)
+                        {
+                            Console.WriteLine("Too High");
+                        }
+                    else 
+                        {
+                            Console.WriteLine("Too Low");
+                        }
+                }
             }
-            catch (Exception)
-            {  
-            Console.WriteLine("You did not enter a valid guess.");
-            guess = GetGuess();
-            }
-            return guess;
         }
         
+
     }
 }
